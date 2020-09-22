6430d1f00254996ad98d0c868b99aaa48c3c506c
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-09-22 13:50:03 +0300

    Implement resolver

--- library/Facade/Components/Resolver.hs
+++ library/Facade/Components/Resolver.hs
@@ -19,14 +19,35 @@ type Eff =
   ExceptT Err ((->) Env)
 
 resolve :: Text -> Eff TypeRef
-resolve =
-  error "TODO"
+resolve a =
+  ExceptT $ \ b ->
+    case HashMap.lookup a b of
+      Just c ->
+        Right c
+      Nothing ->
+        Left ("No definition found for name: " <> a)
 
 type_ :: Doc.Type -> Eff Type
 type_ =
   \ case
     Doc.RefType a ->
       RefType <$> typeRef a
+    Doc.AppType a b ->
+      AppType <$> type_ a <*> type_ b
+    Doc.InSquareBracketsType a ->
+      AppType ListType <$> type_ a
+    Doc.InParensType a ->
+      inParensType a
+
+inParensType :: [Doc.Type] -> Eff Type
+inParensType =
+  \ case
+    [] ->
+      return (TupleType 0)
+    a : [] ->
+      type_ a
+    a ->
+      fmap (foldl' AppType (TupleType (length a))) (traverse type_ a)
 
 typeRef :: Doc.TypeRef -> Eff TypeRef
 typeRef (Doc.TypeRef a) =
@@ -39,3 +60,45 @@ typeRef (Doc.TypeRef a) =
           return (GlobalTypeRef b c)
     Nothing ->
       throwE "Broken type ref"
+
+byTypeName :: Doc.ByTypeName a -> (Text -> a -> Eff b) -> Eff [b]
+byTypeName (Doc.ByTypeName a) b =
+  traverse (\ (c, d) -> b c d) a
+
+byTypeNameTypeDec :: Doc.ByTypeName a -> (a -> Eff TypeDef) -> Eff [Dec]
+byTypeNameTypeDec (Doc.ByTypeName a) b =
+  traverse (\ (c, d) -> TypeDec c <$> b d) a
+
+aliasDef :: Doc.AliasDef -> Eff TypeDef
+aliasDef (Doc.AliasDef a) =
+  AliasTypeDef <$> type_ a
+
+wrapperDef :: Doc.WrapperDef -> Eff TypeDef
+wrapperDef (Doc.WrapperDef a) =
+  WrapperTypeDef <$> type_ a
+
+enumDef (Doc.EnumDef a) =
+  pure (EnumTypeDef a)
+
+productDef (Doc.ProductDef a) =
+  a & typeByFieldName & fmap (CompositeTypeDef ProductComposition)
+
+sumDef (Doc.SumDef a) =
+  a & typeByFieldName & fmap (CompositeTypeDef SumComposition)
+
+typeByFieldName (Doc.TypeByFieldName a) =
+  a & traverse (traverse type_)
+
+doc :: Doc.Doc -> Eff [Dec]
+doc (Doc.Doc a b c d e f) =
+  fmap concat $ sequence [
+    byTypeNameTypeDec b aliasDef
+    ,
+    byTypeNameTypeDec c wrapperDef
+    ,
+    byTypeNameTypeDec d enumDef
+    ,
+    byTypeNameTypeDec e productDef
+    ,
+    byTypeNameTypeDec f sumDef
+    ]
