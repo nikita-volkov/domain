34f5558f9d83c7c0c02528e740b7c907270cc414
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-09-28 16:30:52 +0300

    Correct typeRef rendering

--- library/Domain/ModelText.hs
+++ library/Domain/ModelText.hs
@@ -10,7 +10,10 @@ import qualified Data.Char as Char
 typeRef =
   \ case
     LocalTypeRef a -> a
-    GlobalTypeRef a b -> Text.intercalate "." a <> "." <> b
+    GlobalTypeRef a b ->
+      if null a
+        then b
+        else Text.intercalate "." a <> "." <> b
 
 recordField a b =
   mapFirstChar Char.toLower a <> mapFirstChar Char.toUpper b
